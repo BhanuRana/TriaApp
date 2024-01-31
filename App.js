4bfcc7e52e461708a58cb976bb1f0ce0909038bd
Bhanu Rana <bhanurana@UNBLR-MIC-03266.local>
2024-01-31 13:00:39 +0530

    android app-icon and splash-screen

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import AppNavigator from './src/navigation/AppNavigator';
 import {UserProvider} from './src/context/UserContext';
 import Colors from './src/styles/colors';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <UserProvider>
       <StatusBar backgroundColor={Colors.background} barStyle="light-content" />
